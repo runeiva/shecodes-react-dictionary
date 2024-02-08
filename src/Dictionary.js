3a03afc364578ccb35728cbe3bcbadfbd655f88e
Anah Cordeiro <114919412+anah-cordeiro@users.noreply.github.com>
2024-02-08 14:30:45 +0000

    Added definitions with API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,13 @@
 import React, { useState }from "react";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
 let [keyword, setKeyword] = useState("");
+let [results, setResults] = useState(null);
 
 function handleResponse(response) {
-    console.log(response.data);
+    setResults(response.data);
 }
 
 function search(event) {
@@ -27,6 +29,7 @@ function handleKeywordChange(event) {
             <form onSubmit={search}>
                 <input type="search" onChange={handleKeywordChange}/>
             </form>
+            <Results results={results} />
         </div>
     )
 }
