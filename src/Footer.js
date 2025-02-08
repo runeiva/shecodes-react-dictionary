e67bdaa815c96ee5845f05cf6e1d4d8062781bcc
runeiva <114919412+runeiva@users.noreply.github.com>
2025-02-08 22:33:51 +0000

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ export default function Footer () {
     return (
         <div className="Footer">
             <footer>
-                Coded by Ana Cordeiro. Hosted on <a href={"https://merry-beignet-4981ad.netlify.app"}>Netlify</a>. Repository on <a href={"https://github.com/anah-cordeiro/shecodes-react-dictionary"}>Github</a>.
+                Coded by Runeiva. Hosted on <a href={"https://webdictionary-app.netlify.app/"}>Netlify</a>.
             </footer>
 
         </div>
